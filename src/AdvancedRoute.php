17b7479a1a436969b9cd2cb6f01176014720983e
joesforn <willardmatthew@yahoo.com>
2017-01-17 16:42:33 -0500

    Update AdvancedRoute.php

--- src/AdvancedRoute.php
+++ src/AdvancedRoute.php
@@ -29,7 +29,7 @@ class AdvancedRoute {
             }
         }
 
-        Route::get($path, $controllerClassName . '@index');
+        //Route::get($path, $controllerClassName . '@index');
     }
 
     protected static function stringStartsWith($string, $match) {
